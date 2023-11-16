643ed839c5a29204ecc975eefa92e408a86f8541
enzosamojedny <enzosamojedny@gmail.com>
2023-11-16 00:20:47 -0300

    RAMA MEDIA PILA

--- src/components/Filters/ForumFilters/Forum_DateFilters/Forum_DateInput/Forum_DateInput.jsx
+++ src/components/Filters/ForumFilters/Forum_DateFilters/Forum_DateInput/Forum_DateInput.jsx
@@ -3,7 +3,7 @@ import "react-datepicker/dist/react-datepicker.css";
 //
 import { format } from "date-fns";
 import DatePicker from "react-datepicker";
-import CleanDate_Icon from "../../../../../assets/CleanDate_Icon";
+import CleanDateIcon from "../../../../../assets/CleanDateIcon";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from 'react';
 //
@@ -55,7 +55,7 @@ export default function ({ fromDate, untilDate, handleFromDate, handleUntilDate 
       </button>
 
       <div className={Styles.DateInput__Icon}>
-        <CleanDate_Icon className={Styles.icon} onClick={handleReset} />
+        <CleanDateIcon className={Styles.icon} onClick={handleReset} />
       </div>
 
       <div className={Styles.DateInput__datePicker}>
