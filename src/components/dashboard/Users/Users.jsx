588b1babe42eea00de421a6b1dab2657729c5eb6
enzosamojedny <enzosamojedny@gmail.com>
2023-11-17 00:17:06 -0300

    asdasd

--- src/components/dashboard/Users/Users.jsx
+++ src/components/dashboard/Users/Users.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, } from 'react'
 import Styles from './users.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUsers } from '../../../Redux/actions/usersActions'
-import UserCard from './userCard'
+import UserCard from './UserCard'
 
 
 const Users = () => {
